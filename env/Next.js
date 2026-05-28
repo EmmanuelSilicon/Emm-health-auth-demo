94f1156566f6638acdb93787db8f0393ea330935
EmmanuelSilicon <46695874+EmmanuelSilicon@users.noreply.github.com>
2026-05-28 11:21:58 +0200

    Add files via upload

---
+++ env/Next.js
@@ -0,0 +1,8 @@
+//NEXT_PUBLIC_SUPABASE_URL=https://your-project.supabase.co
+//NEXT_PUBLIC_SUPABASE_ANON_KEY=your-anon-key
+module.exports = {
+  NEXT_PUBLIC_SUPABASE_URL: 'https://eaxcoaoqlpntgsgisbtq.supabase.co',
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVheGNvYW9xbHBudGdzZ2lzYnRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0ODU1ODQsImV4cCI6MjA5NTA2MTU4NH0._g_2ORXOi4SgVC2u2qpYv4RnNosR5fFSzll_zkCDUGE',
+ // NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_2MTkOpdmI5wh0pbsTKxWOA_dKJFGfkw'
+};
+
